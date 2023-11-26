c73417a54048ea46787f5b997c6ae0c5f54394f3
R-Owino <sayeakinyi@gmail.com>
2023-11-26 16:00:16 +0300

    Chore: Source code for the different exercises

---
+++ src/Chapter1/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains the driver code for the
+neural network implementation in the book.
+"""
+
+import mnist_loader
+import network
+
+# Load the MNIST data
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+# Create a network with 784 input neurons, 30 hidden neurons, and 10 output neurons
+net = network.Network([784, 30, 10])
+
+# Train the network using stochastic gradient descent
+# 30 epochs, mini-batch size of 10, and learning rate of 3.0
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
